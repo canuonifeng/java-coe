361716d4c505969f1bd018cf75c432b3491037db
nifeng <nifeng@howzhi.com>
2018-01-29 17:15:12 +0800

    socket

--- src/main/java/com/codeages/socket/SocketClient.java
+++ src/main/java/com/codeages/socket/SocketClient.java
@@ -1,8 +1,8 @@
 package com.codeages.socket;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -12,22 +12,25 @@ public class SocketClient {
         try {
             Socket socket = new Socket("127.0.0.1", 5209);
             System.out.println("客户端启动成功");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            PrintWriter write = new PrintWriter(socket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String readline;
-            readline = br.readLine();
-            while (!readline.equals("end")) {
-                write.println(readline);
-                write.flush();
-                System.out.println("Client:" + readline);
-                System.out.println("Server:" + in.readLine());
-                readline = br.readLine(); // 从系统标准输入读入一字符串
-            }
-
-            write.close();
-            in.close();
-            socket.close();
+			
+			OutputStream out = socket.getOutputStream();
+			PrintWriter printWriter = new PrintWriter(out);
+			printWriter.write("sender message!!!");
+			printWriter.flush();
+			printWriter.close();
+			
+			InputStream in = socket.getInputStream();
+			OutputStream outputStream = System.out;
+			int inRead = 0;
+			while((inRead = in.read()) >= 0) {
+				outputStream.write(inRead);
+			}
+			outputStream.flush();
+			outputStream.close();
+			
+			out.close();
+			in.close();
+			socket.close();
         } catch (Exception e) {
             System.out.println("can not listen to:" + e);// 出错，打印出错信息
         }
--- src/main/java/com/codeages/socket/SocketService.java
+++ src/main/java/com/codeages/socket/SocketService.java
@@ -2,7 +2,9 @@ package com.codeages.socket;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,29 +12,31 @@ import java.net.Socket;
 public class SocketService {
 
 	public static void main(String[] args) throws IOException {
-		SocketService socketService = new SocketService();
-		socketService.oneServer();
-	}
-
-	public void oneServer() {
 		try {
 			ServerSocket server = new ServerSocket(5209);
 			System.out.println("服务器启动成功");
+			
 			Socket socket = server.accept();
+			System.out.println("收到一个连接！！！！");
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter writer = new PrintWriter(socket.getOutputStream());
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String line = br.readLine();
-			while (!line.equals("end")) {
-				writer.println(line);
-				writer.flush();
-				System.out.println("Server:" + line);
-				System.out.println("Client:" + in.readLine());
-				line = br.readLine();
+			InputStream in = socket.getInputStream();
+			OutputStream outputStream = System.out;
+			
+			int inRead = 0;
+			while((inRead = in.read()) >= 0) {
+				outputStream.write(inRead);
 			}
-
-			writer.close();
+			
+			outputStream.flush();
+			outputStream.close();
+			
+			OutputStream out = socket.getOutputStream();
+			PrintWriter printWriter = new PrintWriter(out);
+			printWriter.write("i get!!!!");
+			printWriter.flush();
+			printWriter.close();
+			out.close();
+			
 			in.close();
 			socket.close();
 			server.close();
